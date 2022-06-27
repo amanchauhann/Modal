a0398e2cfbe8f48c8fcbf13516eacff3daeee848
Aman Chauhan <amangotpro@gmail.com>
2022-06-27 10:53:41 +0530

    d

---
+++ main.js
@@ -0,0 +1,8 @@
+document.getElementById("openModal").addEventListener("click", function myFunction() {
+    document.getElementById("modalContainer").style.display="block";
+  })
+  
+  document.getElementById("closeModal").addEventListener("click", function myFunction() {
+    document.getElementById("modalContainer").style.display="none";
+  })
+  
